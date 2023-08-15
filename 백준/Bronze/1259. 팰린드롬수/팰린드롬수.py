a39d0c9131f8a7fc3b53a1da78b3409065b3e623
standyoung <82448471+standyoung@users.noreply.github.com>
2023-08-16 01:33:16 +0900

    [Bronze I] Title: 팰린드롬수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,13 +1,13 @@
 # 1259. 펠린드롬수
-
 while True :
+    flag = True # 입력할 때 마다 초기화 시켜줘야함
     p = input()
     if p == "0":
         break
 
     elif p != "0":
         if len(p) == 1 : # p:1,2,3,4,5...
-            flag = "yes"
+            flag = True
         else :
             if p[0] == "0": # ex) 무의미한 0 맨앞ㄴ
                 flag = "no"
@@ -16,11 +16,12 @@ while True :
                 for i in range(0,s+1,1): # 0~s
                     if p[i] != p[-(i+1)]: # 앞, 뒤 순서대로 비교
                         flag = "no"
-                        break # for문 중단
-                    else :
-                        flag = "yes"
-                    
-    print(flag)
+                    # else :
+                        # break # for문 중단
+    if flag == True :                   
+        print("yes")
+    else :
+        print("no")
 
 # print(1//2) --> 0
 # print(2//2) --> 1
