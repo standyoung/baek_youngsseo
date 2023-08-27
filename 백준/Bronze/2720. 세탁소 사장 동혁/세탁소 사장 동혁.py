546aa959e4ffa5ba646a736f355bbc87b9c114af
standyoung <82448471+standyoung@users.noreply.github.com>
2023-08-27 21:58:53 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,26 +1,12 @@
-# 0.03 // 0.01 = 2.0
-# 0.03 / 0.01 = 3.0
-# 실수를 정수로 바꿀 때 진짜 조심해야함
+# 실수는 정수로 바꾸는게 좋음
 T = int(input())
 
 for _ in range(T):
-    change_lst = []
+    coin_lst = [25,10,5,1]
     C = int(input())
-    C *= 0.01              # C의 단위 센트
 
-    q = int(C / 0.25)
-    change_lst.append(q)
-    C = round(C - (0.25 * q),2)
-
-    d = int(C / 0.10)
-    change_lst.append(d)
-    C = round(C - (0.10 * d),2)
-
-    n = int(C / 0.05)
-    change_lst.append(n)
-    C = round(C - (0.05 * n),2)
-
-    p = int(C / 0.01)
-    change_lst.append(p)
-
-    print(*change_lst)
+    for i in range(len(coin_lst)):
+        change = C // coin_lst[i]   # 거스름돈
+        print(change, end=' ')
+        C %= coin_lst[i]            # 나머지는 다음 거스름돈으로 넘어감
+    print()
