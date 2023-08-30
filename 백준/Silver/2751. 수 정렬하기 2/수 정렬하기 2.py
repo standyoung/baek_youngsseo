ee9f72877d145482d75f11b233f573d7a5d671a8
standyoung <82448471+standyoung@users.noreply.github.com>
2023-08-31 00:36:55 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1356 ms, Memory: 77108 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,12 +1,14 @@
-# sort()와 sorted()의 차이점을 알아둘것..!
 import sys
+
 N = int(sys.stdin.readline())
-num_lst = []
+lst = [int(sys.stdin.readline()) for _ in range(N)]
 
-for _ in range(N):
-    inpt = int(sys.stdin.readline())
-    num_lst.append(inpt)
+lst.sort()
+# sort() : 기존 lst 오름차순으로 정렬
+# sorted() : 기존 lst 냅두고 새롭게 정렬된 리스트로 반환
 
-num_lst.sort()
-
-print(*num_lst, sep='\n')
+for i in range(len(lst)):
+    print(lst[i])
+# print(*lst, sep="\n")
+# * : unpacking operator
+# sep : 구분자
