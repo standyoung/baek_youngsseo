13b8f18111bb94749fb2c48974e8cf27b1047a1e
standyoung <82448471+standyoung@users.noreply.github.com>
2023-08-30 23:24:36 +0900

    [Bronze I] Title: 최소공배수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,22 +1,13 @@
-# 최소 공배수 = 두 수의 곱 / 최대 공약수
-
-T = int(input())
+import sys
+T = int(sys.stdin.readline())
 
 for _ in range(T):
-    fac_lst = []    # 공약수 리스트
-    A,B = map(int, input().split())
+    A,B = map(int, sys.stdin.readline().split())
+    A_, B_ = A, B # 처음 A, B 저장
 
-    if A >= B: 
-        for i in range(1,A):
-            if (A % i) == 0 and (B % i) == 0:
-                fac_lst.append(i)
-    else :
-        for i in range(1,B):
-            if (A % i) == 0 and (B % i) == 0:
-                fac_lst.append(i)
-
-    if A == B:  # 같은 수가 들어올 때
-        print(A)
-    else:
-        max_fac = max(fac_lst)
-        print(int((A*B) / max_fac))
+    while(B>0):
+        A, B = B, A % B
+        # A = 최대 공약수 -> 유클리드 호제법 이용
+        # 최대 공배수 -> 두 수의 곱 // 최대 공약수
+    
+    print((A_*B_)//A)
