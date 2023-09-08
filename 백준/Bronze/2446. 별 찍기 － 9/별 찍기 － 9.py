10b9e6ac39beb0a0084e992322c2821e4c1cda62
standyoung <82448471+standyoung@users.noreply.github.com>
2023-09-08 09:20:09 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 48 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
@@ -0,0 +1,7 @@
+N = int(input())
+
+for i in range(1, N + 1):
+    print(" " * (i - 1) + "*" * (2 * N - 2 * i + 1))
+
+for i in range(N - 1, 0, -1):
+    print(" " * (i - 1) + "*" * (2 * N - 2 * i + 1))
