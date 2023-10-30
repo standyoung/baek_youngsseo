9605713a9b8950e8973b46487f36fa4402fca3f7
standyoung <82448471+standyoung@users.noreply.github.com>
2023-10-30 23:19:16 +0900

    [Bronze V] Title: A+B - 9, Time: 420 ms, Memory: 20612 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger A = sc.nextBigInteger();
+        BigInteger B = sc.nextBigInteger();
+        System.out.println(A.add(B));
+
+    }
+}
+
