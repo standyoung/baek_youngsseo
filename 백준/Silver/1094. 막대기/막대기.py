1bd9590c04451f67469f7839cc1f000e68089e60
standyoung <82448471+standyoung@users.noreply.github.com>
2024-01-03 16:59:07 +0900

    [Silver V] Title: 막대기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.py
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -1,33 +1,15 @@
+# 다른 참고 풀이 : https://sodehdt-ldkt.tistory.com/216
 x = int(input()) # x : 가지고 싶은 막대 길이
-x_lst = [64]
 
-flag = 0
+stick = [64,32,16,8,4,2,1]
 
-if x == 64:
-    print(1)
-    exit(0)
+cnt = 0
 
-# 64 / 2 => 32 32, 32 > 15 => 32
-# 32 / 2 => 16 16, 16 > 15 => 16
-# 16 / 2 => 8 8,  8 < 15 => 8,8
-# 8 / 2 => 8 4 4, 12 < 15 => 8,4,4
-# 4 / 2 => 8 4 2 2, 16 > 15 => 8,4,2
-# 2 / 2 => 8 3 2 1 1, 15 = 15 => 8,4,2,1
-i=0
-while sum(x_lst) >= x:
-    cut = (min(x_lst)//2) # 32
-
-    if len(x_lst) > 1:
-        x_lst[-1] = cut
-        x_lst.append(cut)
-    else :
-        x_lst[0] = cut
-        x_lst.append(cut)
-
-    if sum(x_lst[:(len(x_lst)-1)]) >= x:
-        x_lst.pop(-1)
-    
-    if sum(x_lst) == x:
-        print(len(x_lst))
+for i in range(len(stick)):
+    if x==0:
         break
-    
+    if stick[i] <= x:
+        x -= stick[i]
+        cnt += 1
+
+print(cnt)
